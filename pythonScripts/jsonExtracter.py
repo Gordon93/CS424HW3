360f9d9b46e272a852ed530820fc6631d896b544
ryan jones <ryan.nolan.jones.53@gmail.com>
2015-11-05 20:12:21 -0600

    Sample data

--- pythonScripts/jsonExtracter.py
+++ pythonScripts/jsonExtracter.py
@@ -1,40 +1,126 @@
 import urllib.request
 import json
 import csv
+import string
+import sys
+
+
+
 
 #genreUrl = 'http://developer.echonest.com/api/v4/genre/list?api_key=DLSWESE3XURBC96V4&format=json'
 listUrl = 'http://developer.echonest.com/api/v4/artist/search?api_key=DLSWESE3XURBC96V4&'
-buckets = '&bucket=hotttnesss&bucket=genre&bucket=years_active&bucket=discovery&bucket=familiarity&results=100&start='
+buckets = '&bucket=hotttnesss&bucket=artist_location&bucket=years_active&bucket=discovery&bucket=familiarity&results=100&start='
 
 
 #response = urllib.request.urlopen(genreUrl)
 #content = response.read()
 #json_str = json.dumps(content)
 #data = json.loads(content.decode())
-headers = ["artist","genre","Years Active","Hotttnesss","Discovery","Familiarity"]
+headers = ["artist","genre","Start","End","Location","Hotttnesss","Discovery","Familiarity"]
 genre = []
 yearsActive = []
+locKey = 'artist_location'
+rlocKey = 'location'
+
+nameKey = 'name'
+hotKey = 'hotttnesss'
+actKey = 'years_active'
+famKey = 'familiarity'
+disKey = 'discovery'
+
+
+def spaces(genre):
+    retname = ''
+    for char in genre:
+        if char == ' ':
+            retname = retname + '%20'
+        else:
+            retname = retname + char
+    return retname
+def isAscii(word):
+    for char in word:
+        if char not in string.ascii_letters:
+            return False
+
+    return True
+
+
 
 reader = csv.reader(open('genres.csv'))
-writable = csv.writer(open('output.txt','w',newline=''))
-artist = []
-artist.append(headers)
+writable = csv.writer(open('output.csv','w',encoding='utf-8',newline=''))
+#open('output.txt','w',newline='')
+artistList = []
+artistList.append(headers)
 debug = True
 for genre in reader:
-    style = genre[0]
+    genreName = genre[0]
+    genreName = "rap"
+    style = spaces(genreName)
     #print(style)
     genreLIstUrl = listUrl+"style="+style+buckets
-    start = 0
+    startR = 0
     if debug==True:
-        while start<100 :
-            print(style)
-            url = genreLIstUrl + str(start)
+        while startR<1000 :
+            #print(style)
+            url = genreLIstUrl + str(startR)
             print(url)
             response = urllib.request.urlopen(url)
             content = response.read()
             data = json.loads(content.decode('utf-8'))
             for artist in data ['response']['artists']:
-                #print(artist['name'])
+                artistData = []
+                start = 9999
+                end = 9999
+                if nameKey in artist:
+                    #if isAscii(artist[nameKey])==True:
+                        name = artist[nameKey]
+                        artistData.append(artist[nameKey])
+                        artistData.append(genreName)
+                        if locKey in artist:
+                            #print(artist['artist_location'])
+                            artistData.append( artist[locKey][rlocKey])
+                        else:
+                            artistData.append("none")
+
+                        if actKey in artist:
+                            for years in artist[actKey]:
+                                if 'start' in years:
+                                    start = years['start']
+                                else :
+                                    start = 9999
+                                if 'end' in years:
+                                    end = years['end']
+                                else:
+                                    end = 9999
+                            artistData.append(start)
+                            artistData.append(end)
+                        else:
+                            artistData.append(0)
+                            artistData.append(0)
+
+                        if famKey in artist:
+                            artistData.append(artist[famKey])
+                        else:
+                            artistData.append(0)
+                        if hotKey in artist:
+                            artistData.append(artist[hotKey])
+                        else:
+                            artistData.append(0)
+                        if disKey in artist:
+                            artistData.append(artist[disKey])
+                        else:
+                            artistData.append(0)
+                        #print(artistData)
+                        artistList.append(artistData)
+
+            startR = startR + 100
+
+        debug = False
+        for artist in artistList:
+            writable.writerow(artist)
+
+
+
                 #print(data['response']['artists'][0]['name'])
                 #print(data['response']['artists'][0]['genres'][0]['name'])
                 #print(data['response']['artists'][0]['familiarity'])
@@ -42,8 +128,10 @@ for genre in reader:
                 #print(data['response']['artists'][0]['years_active'])
                 #print(data['response']['artists'][0]['discovery'])
 
-            start = start + 100
-    debug = False
+
+
+
+
 
 
 #print(data['response']['genres'][0]['name'])
