245beae24e48d07e53d4feb4a948e87818993425
Ryan <rjones32@uic.edu>
2015-11-10 15:17:13 -0600

    updated data for 10 artist for each decade

--- pythonScripts/jsonExtracter3.py
+++ pythonScripts/jsonExtracter3.py
@@ -8,17 +8,17 @@ import sys
 
 
 
+top10Url = 'http://developer.echonest.com/api/v4/artist/search?api_key=DLSWESE3XURBC96V4&format=json&sort=hotttnesss-desc&bucket=hotttnesss&bucket=genre&results=10&'
+endUrl = 'artist_start_year_before='
+startUrl   = 'artist_start_year_after='
 
-#genreUrl = 'http://developer.echonest.com/api/v4/genre/list?api_key=DLSWESE3XURBC96V4&format=json'
-listUrl = 'http://developer.echonest.com/api/v4/artist/search?api_key=DLSWESE3XURBC96V4&'
-buckets = '&bucket=hotttnesss&bucket=artist_location&bucket=years_active&bucket=discovery&bucket=familiarity&results=100&start='
 
 
 #response = urllib.request.urlopen(genreUrl)
 #content = response.read()
 #json_str = json.dumps(content)
 #data = json.loads(content.decode())
-headers = ["artist","ID","genre","Location","Start","End","Hotttnesss","Discovery","Familiarity"]
+headers = ["DecadeStart","DecadeEnd","Name","Location","Start","End","Hotttnesss","Discovery","Familiarity"]
 genre = []
 yearsActive = []
 locKey = 'artist_location'
@@ -48,130 +48,69 @@ def isAscii(word):
 
 
 
-reader = csv.reader(open('genre4.csv'))
-fp = open('output.csv','a',encoding='utf-8',newline='')
+
+fp = open('output2.csv','w',encoding='utf-8',newline='')
 writable = csv.writer(fp)
 #open('output.txt','w',newline='')
-artistList = []
+top10List = []
 #artistList.append(headers)
 debug = True
 count = 0
-for genre in reader:
-    if count == 10:
+year = 1899
+while(year<=2010):
+    print(str(year))
+    if count == 5:
         count = 0
+        #debug = False
         print("Going to sleep for 60 secs")
         time.sleep(60)
+    url = top10Url+startUrl+str(year)+'&'+endUrl+str(year+11)
 
-    #genreName = genre[0]
-    genreName = "Rock"
-    style = spaces(genreName)
-    #print(style)
-    genreLIstUrl = listUrl+"style="+style+buckets
-    startR = 0
     if debug==True:
-        while startR<1000 :
-            #print(style)
-            url = genreLIstUrl + str(startR)
-            print(url)
-            try:
-                response = urllib.request.urlopen(url)
-                count = count +1
-                content = response.read()
-                data = json.loads(content.decode('utf-8'))
-                for artist in data ['response']['artists']:
-                    artistData = []
-                    start = 9999
-                    end = 9999
-                    if nameKey in artist:
-                        #if isAscii(artist[nameKey])==True:
-                            name = artist[nameKey]
-                            artistData.append(artist[nameKey])
-                            artistData.append(artist['id'])
-                            artistData.append(genreName)
-                            if locKey in artist:
-                                #print(artist['artist_location'])
-                                artistData.append( artist[locKey][rlocKey])
-                            else:
-                                artistData.append("none")
-
-                            if actKey in artist:
-                                for years in artist[actKey]:
-                                    if 'start' in years:
-                                        start = years['start']
-                                    else :
-                                        start = 9999
-                                    if 'end' in years:
-                                        end = years['end']
-                                    else:
-                                        end = 9999
-                                artistData.append(start)
-                                artistData.append(end)
-                            else:
-                                artistData.append(0)
-                                artistData.append(0)
-
-                            if famKey in artist:
-                                artistData.append(artist[famKey])
-                            else:
-                                artistData.append(0)
-                            if hotKey in artist:
-                                artistData.append(artist[hotKey])
-                            else:
-                                artistData.append(0)
-                            if disKey in artist:
-                                artistData.append(artist[disKey])
-                            else:
-                                artistData.append(0)
-                            #print(artistData)
-                            artistList.append(artistData)
-            except urllib.error.HTTPError as e:
-                print(e.code)
-                print(e.read())
-            except urllib.error.URLError as e:
-                print(e.code)
-                print(e.read())
+        print(url)
+        try:
+            response = urllib.request.urlopen(url)
+            count = count +1
+            content = response.read()
+            data = json.loads(content.decode('utf-8'))
+            for artist in data ['response']['artists']:
+                #print("here")
+                artistData = []
+                if nameKey in artist:
+                    #if isAscii(artist[nameKey])==True:
+                    name = artist[nameKey]
+                    artistData.append(str(year))
+                    artistData.append(str(year+11))
+                    artistData.append(artist[nameKey])
+                    artistData.append(artist['id'])
+                    #artistData.append(artist['genres'])
+                    if hotKey in artist:
+                        artistData.append(artist[hotKey])
+                    else:
+                        artistData.append(0)
+                    #print(artistData)
+                    top10List.append(artistData)
+        except urllib.error.HTTPError as e:
+            print(e.code)
+            print(e.read())
+        except urllib.error.URLError as e:
+            print(e.code)
+            print(e.read())
 
 
 
-            startR = startR + 100
+        if count == 5:
+            count = 0
+            #debug = False
+            print("sleep for 60 secs")
+            time.sleep(60)
 
-            if count == 10:
-                count = 0
-                print("sleep for 60 secs")
-                time.sleep(60)
+    year = year+10
+    for top10 in top10List:
+        writable.writerow(top10)
 
-        debug = False
-        for artist in artistList:
-            writable.writerow(artist)
+    fp.close()
+    fp = open('output2.csv',mode='a',encoding='utf-8',newline='')
+    writable = csv.writer(fp)
+    top10List = []
 
-        fp.close()
-        fp = open('output.csv',mode='a',encoding='utf-8',newline='')
-        writable = csv.writer(fp)
-        artistList = []
-
-                #print(data['response']['artists'][0]['name'])
-                #print(data['response']['artists'][0]['genres'][0]['name'])
-                #print(data['response']['artists'][0]['familiarity'])
-                #print(data['response']['artists'][0]['hotttnesss'])
-                #print(data['response']['artists'][0]['years_active'])
-                #print(data['response']['artists'][0]['discovery'])
-
-
-
-
-
-
-
-#print(data['response']['genres'][0]['name'])
-
-#for genre in data['response']['genres']:
- #   name = [genre['name']]
-  #  print(name)
-   # genres.append(name)
-
-
-#print(genres)
-#for name in genres:
- #   print(name)
-#for genre in genres:
- #       writable.writerow(genre)
